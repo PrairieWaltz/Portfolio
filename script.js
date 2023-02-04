c9ee1a086eb10ddda027e31844ad56391302ca9f
PrairieWaltz <minnowvswhale@gmail.com>
2023-02-04 14:54:21 -0500

    emailConnect

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 'use strict';
+
+// const emailInput = document.getElementById('email').value;
+
+// function sendEmail() {
+//   Email.send({
+//     Host: '',
+//     Username: '',
+//     Password: '',
+//     To: '',
+//     From: '',
+//     Subject: 'hi',
+//     Body: 'And this is the body',
+//   }).then(message => alert(message));
+//   console.log(emailInput);
+// }
