7680bc23d138596047725acab887e53684bae51f
PrairieWaltz <minnowvswhale@gmail.com>
2023-02-11 11:23:29 -0500

    responsiveRefactor

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-// const emailInput = document.getElementById('email').value;
+// Get the modal
+let modal = document.getElementById('myModal');
 
-// function sendEmail() {
-//   Email.send({
-//     Host: '',
-//     Username: '',
-//     Password: '',
-//     To: '',
-//     From: '',
-//     Subject: 'hi',
-//     Body: 'And this is the body',
-//   }).then(message => alert(message));
-//   console.log(emailInput);
-// }
+// Get the button that opens the modal
+let btn = document.getElementById('myBtn');
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName('close')[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function () {
+  modal.style.display = 'block';
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = 'none';
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = 'none';
+  }
+};
